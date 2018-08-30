931aee1e94eb95709d98aa9ad7cdae560fb648bc
Travis Nguyen <travishn@uci.edu>
2018-08-29 17:38:21 -0700

    Create ReactDOM render using root id

---
+++ frontend/patient_journey.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const patientJourney = {
+    'OR': {
+      'duration': 240, // these values are in minutes
+      'events': ['Anesthesia Start', 'Patient ready to transfer to CVICU']
+    },
+    'CVICU': {
+      'duration': 4620,
+      'events': ['Transfer', 'Foley, Central Line', 'Patient ready to transfer to Acute Care']
+    },
+    'Acute Care': {
+      'duration': 14760,
+      'events': ['Transfer', 'Ready to Discharge', 'Discharge']
+    },
+    'Home': {
+      'duration': 0,
+      'events': []
+    }
+  };
+  
+  ReactDOM.render(<App journeyData={patientJourney} />, document.getElementById('root'));
+});
