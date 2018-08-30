1e315a3d3399717764a8a34645dbc51b8ab4558e
Travis Nguyen <travishn@uci.edu>
2018-08-29 17:25:17 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/patient_journey.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: [/\.jsx?$/, /\.js?$/],
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      query: {
+        presets: ['env', 'react']
+      }
+    }]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
